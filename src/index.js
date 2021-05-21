bc88f8ec1e2b90744f95b4e56b36f4ee24dd8dda
Oleksandr Tarasiuk <oleksandr.tarasyuk92@gmail.com>
2021-05-21 23:47:54 +0200

    фініш

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './sass/main.scss';
 import menu from './menu.json';
 import template from './templates/menu-template.hbs';
 
-// const STORAGE_KEY = 'changeTheme';
+const STORAGE_KEY = 'changeTheme';
 
 const refs = {
   themeSwitchCheckbox: document.querySelector('#theme-switch-toggle'),
@@ -10,34 +10,43 @@ const refs = {
   menuTemplate: document.querySelector('.menu.js-menu'),
 };
 
-const cardMarkup = template(menu);
-
-refs.menuTemplate.insertAdjacentHTML('beforeend', cardMarkup);
-refs.themeSwitchCheckbox.addEventListener('change', switchCheckbox);
-
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
 
+loadPage();
+
+const cardMarkup = template(menu);
+
+refs.menuTemplate.insertAdjacentHTML('beforeend', cardMarkup);
+refs.themeSwitchCheckbox.addEventListener('change', switchCheckbox);
+
 function switchCheckbox(evt) {
   if (refs.themeSwitchCheckbox.checked) {
-    switchColorTheme();
+    switchColorDark();
   } else {
-    switchColorTheme();
+    switchColorLight();
   }
 }
 
-function switchColorTheme() {
-  console.log('DArk');
+function switchColorDark() {
   refs.myBodyTheme.classList.remove(Theme.LIGHT);
-  //   localStorage.setItem(STORAGE_KEY, Theme.DARK);
+  localStorage.setItem(STORAGE_KEY, Theme.DARK);
   refs.myBodyTheme.classList.add(Theme.DARK);
-  refs.themeSwitchCheckbox.checked = true;
 }
 
-function switchColorTheme() {
+function switchColorLight() {
   refs.myBodyTheme.classList.remove(Theme.DARK);
-  //   localStorage.setItem(STORAGE_KEY, Theme.LIGHT);
+  localStorage.setItem(STORAGE_KEY, Theme.LIGHT);
   refs.myBodyTheme.classList.add(Theme.LIGHT);
+}
+
+function loadPage() {
+  const currentTheme = localStorage.getItem(STORAGE_KEY);
+  refs.myBodyTheme.classList.add(currentTheme);
+
+  if (currentTheme === Theme.DARK) {
+    refs.themeSwitchCheckbox.checked = true;
+  }
 }
