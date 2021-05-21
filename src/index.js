2f3b37d182748bf17267873ac859a9e427febf86
Oleksandr Tarasiuk <oleksandr.tarasyuk92@gmail.com>
2021-05-21 23:10:05 +0200

    dgxfhcgjvjgv

--- src/index.js
+++ src/index.js
@@ -1,43 +1,43 @@
-// import './sass/main.scss';
-// import menu from './menu.json';
-// import template from './templates/color-template.hbs';
-// const THEME_KEY = 'myTheme';
-// const CHECK_BOX = 'myCheckbox';
+import './sass/main.scss';
+import menu from './menu.json';
+import template from './templates/menu-template.hbs';
 
-// const refs = {
-//   switchCheckbox: document.querySelector('#theme-switch-toggle'),
-//   myBodyThem: document.querySelector('body'),
-//   menuTemplate: document.querySelector('.menu.js-menu'),
-// };
+// const STORAGE_KEY = 'changeTheme';
 
-// const Theme = {
-//   LIGHT: 'light-theme',
-//   DARK: 'dark-theme',
-// };
-// if (localStorage.getItem(THEME_KEY) == 'dark-theme') {
-//   refs.switchCheckbox.checked = true;
-// }
+const refs = {
+  themeSwitchCheckbox: document.querySelector('#theme-switch-toggle'),
+  myBodyTheme: document.querySelector('body'),
+  menuTemplate: document.querySelector('.menu.js-menu'),
+};
 
-// const markup = template(menu);
-// refs.menuTemplate.insertAdjacentHTML('beforeend', markup); //шаблонізація
-// refs.myBodyThem.classList.add(localStorage.getItem(THEME_KEY));
-// refs.switchCheckbox.addEventListener('change', funCheckbox);
+const cardMarkup = template(menu);
 
-// function funCheckbox(e) {
-//   if (refs.switchCheckbox.checked) {
-//     funCheckboxTrue();
-//   } else {
-//     funCheckboxFalse();
-//   }
-// }
-// function funCheckboxTrue() {
-//   refs.myBodyThem.classList.remove(localStorage.getItem(THEME_KEY));
-//   localStorage.setItem(THEME_KEY, Theme.DARK);
-//   refs.myBodyThem.classList.add(localStorage.getItem(THEME_KEY));
-//   refs.switchCheckbox.checked = true;
-// }
-// function funCheckboxFalse() {
-//   refs.myBodyThem.classList.remove(localStorage.getItem(THEME_KEY));
-//   localStorage.setItem(THEME_KEY, Theme.LIGHT);
-//   refs.myBodyThem.classList.add(localStorage.getItem(THEME_KEY));
-// }
+refs.menuTemplate.insertAdjacentHTML('beforeend', cardMarkup);
+refs.themeSwitchCheckbox.addEventListener('change', switchCheckbox);
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+function switchCheckbox(evt) {
+  if (refs.themeSwitchCheckbox.checked) {
+    switchColorTheme();
+  } else {
+    switchColorTheme();
+  }
+}
+
+function switchColorTheme() {
+  console.log('DArk');
+  refs.myBodyTheme.classList.remove(Theme.LIGHT);
+  //   localStorage.setItem(STORAGE_KEY, Theme.DARK);
+  refs.myBodyTheme.classList.add(Theme.DARK);
+  refs.themeSwitchCheckbox.checked = true;
+}
+
+function switchColorTheme() {
+  refs.myBodyTheme.classList.remove(Theme.DARK);
+  //   localStorage.setItem(STORAGE_KEY, Theme.LIGHT);
+  refs.myBodyTheme.classList.add(Theme.LIGHT);
+}
